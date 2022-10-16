2edcb9bee21446b8bb89bfa431c9f711a5bbb96d
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-10-16 20:45:32 +0200

    [BREWERY] - initial app state

---
+++ brewery/src/main/java/cloud/cholewa/reactive/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.reactive.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryApplication.class, args);
+    }
+
+}
